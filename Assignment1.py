28f18abd6a6696e07700e12831a62da1a82cd09d
Bhupesh <gundabhupesh111@gmail.com>
2021-06-02 22:03:30 +0530

    initialized distance and number of stops to 0

--- Assignment1.py
+++ Assignment1.py
@@ -2,3 +2,8 @@ def routes():
     # add edges as list with weight
     edges = [['A', 'B', 5], ['B', 'C', 4], ['C', 'D', 8], ['D', 'C', 8], ['D', 'E', 6], ['A', 'D', 5], ['C', 'E', 2],
              ['E', 'B', 3], ['A', 'E', 7]]
+    route = ['A', 'D', 'C']
+    distance = 0  # initialize distance to 0
+    routestops = 0  # initialize number of stops to 0
+    sourceNode = route[routestops]
+    destNode = route[routestops + 1]
